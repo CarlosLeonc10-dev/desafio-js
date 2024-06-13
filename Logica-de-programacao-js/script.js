3ff78d6d554ea736a4f27d9a7fecc982abb2e125
CarlosLeonc10-dev <carlosleok17@gmail.com>
2024-06-13 18:26:24 -0300

    Add files via upload

---
+++ Logica-de-programacao-js/script.js
@@ -0,0 +1,23 @@
+let numberOne = Number(prompt("Digite um número"))
+
+let numberTwo = Number(prompt("Digite o segundo número"))
+
+sum = numberOne + numberTwo
+
+alert(`A soma é ${numberTwo + numberTwo}`) 
+alert(`A sbtração  é ${sum}`)
+alert(`A multiplicação é ${numberTwo * numberTwo}`)
+alert(`A divisão é ${numberTwo / numberTwo}`)
+alert(`A resto é ${numberTwo % numberTwo}`)
+
+if(sum % 2 == 0){
+ alert(' a soma dos dois números é par')
+}else{
+ alert(' a soma dos dois números é impar')
+}
+
+if(numberOne == numberTwo){
+ alert("dois números inseridos são iguais")
+}else{
+ alert("dois números inseridos são diferentes")
+}
